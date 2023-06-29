a35628f358e86ecef90b7e45b2fc39a16b68fa99
Y <hwiyul_g@daum.net>
2023-06-30 00:34:02 +0900

    Update : MainActivity(AdapterPager2를 가진 부분)에 viewPager(화면상의 요소)를 담아 해당 ViewPager의 adapter에 내가 FragmentStateAdapter를 상속받아 만든 Simple Adatper 장착

--- app/src/main/java/com/example/swipe_nav_pratice_android/MainActivity.kt
+++ app/src/main/java/com/example/swipe_nav_pratice_android/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.swipe_nav_pratice_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.pager)
+
+        val pagerAdapter = SimpleFragmentAdapter(this)
+        viewPager.adapter = pagerAdapter
     }
+
+
 }
